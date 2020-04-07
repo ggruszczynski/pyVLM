sign fix

Solver/llt_sail_opt_solver.py
@@ -11,17 +11,18 @@ def assembly_sys_of_eq_llt(V_app_infs, panels):
     ctr_points = np.array([panel.get_ctr_point_position() for panel in panels.flatten()])
 
     for i in range(0, N):
-        RHS[i] = V_app_infs[i][1]*panels1D[i].get_panel_span()
+        RHS[i] = V_app_infs[i][1] * panels1D[i].get_panel_span()
         for j in range(0, N):
-                # velocity induced at i-th control point by j-th vortex
-                v_ind_coeff[i][j] = panels1D[j].get_horse_shoe_induced_velocity(ctr_points[i], V_app_infs[j])
+            # velocity induced at i-th control point by j-th vortex
+            v_ind_coeff[i][j] = panels1D[j].get_horse_shoe_induced_velocity(ctr_points[i], V_app_infs[j])
 
     for i in range(0, N):
         for j in range(0, N):
-            A[i][j] = v_ind_coeff[i][j][1]*panels1D[j].get_panel_span() \
-                      + v_ind_coeff[j][i][1]*panels1D[i].get_panel_span()
+            A[i][j] = (v_ind_coeff[i][j][1] * panels1D[j].get_panel_span() \
+                       + v_ind_coeff[j][i][1] * panels1D[i].get_panel_span())
 
     return A, RHS, v_ind_coeff  # np.array(v_ind_coeff)
+
 
 def calc_circulation_llt(V_app_infs, panels):
     # it is assumed that the freestream velocity is V [vx,vy,0], where vx > 0  # TODO: ensure that it is right - different csys in case of rectangular wing?

examples/sail_optimization.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from Solver.vlm_solver import calc_induced_velocity
+from Solver.vlm_solver import calc_induced_velocity, is_no_flux_BC_satisfied
 from Solver.mesher import make_panels_from_points
 from Solver.geometry_calc import rotation_matrix
 from Solver.llt_sail_opt_solver import calc_circulation_llt
@@ -79,14 +79,14 @@ tws_at_ctr_points = np.array([winds.get_true_wind_speed_at_h(abs(ctr_point[2])) 
 V_app_infs = np.array([winds.get_app_infs_at_h(tws_at_ctr_point) for tws_at_ctr_point in tws_at_ctr_points])
 
 gamma_magnitude, v_ind_coeff = calc_circulation_llt(V_app_infs, panels)
-V_induced = calc_induced_velocity(v_ind_coeff, gamma_magnitude)
-V_app_fs = V_app_infs + V_induced
 
+V_induced = calc_induced_velocity(v_ind_coeff, gamma_magnitude)
+V_app_fs = V_app_infs - V_induced
 spans = np.array([panel.get_panel_span() for panel in panels1D])
 Thrust_inviscid_per_Panel = V_app_fs[:, 1] * gamma_magnitude * spans * rho
-Thrust_inviscid_total = sum(Thrust_inviscid_per_Panel)
+Thrust_inviscid_total = sum(Thrust_inviscid_per_Panel)/2
 print(f"Thrust in the direction of yacht movement (including leeway) without profile drag = {Thrust_inviscid_total} [N]")
-# assert is_no_flux_BC_satisfied(V_app_fw, panels)
+# assert is_no_flux_BC_satisfied(V_app_fs, panels)
 
 ### Calculate chord assuming CL
 
@@ -100,8 +100,8 @@ alfa_0 = -2.0 * np.pi/180
 
 alfa_app_fs = np.arctan(V_app_fs[:, 1]/V_app_fs[:, 0])
 alfa_app_infs = np.array([winds.get_app_alfa_infs_at_h(tws_at_ctr_point) for tws_at_ctr_point in tws_at_ctr_points])
-alfa_ind = alfa_app_fs - alfa_app_infs
-phi = alfa_app_infs - alfa_0 - CL/a  # sail_twist eq.2.22 from GG
+alfa_ind = alfa_app_infs - alfa_app_fs
+phi = alfa_app_fs - alfa_0 - CL/a  # sail_twist eq.2.22 from GG
 phi_deg = np.rad2deg(phi)
 # phi_boom = phi[0] - phi # take the middle one
 

tests/test_llt_sail.py
@@ -6,11 +6,12 @@ from unittest import TestCase
 from Solver.mesher import make_panels_from_points
 from Solver.winds import Winds
 from Solver.llt_sail_opt_solver import calc_circulation_llt
+from Solver.vlm_solver import calc_induced_velocity
 from Solver.geometry_calc import rotation_matrix
 
 
 class TestForces(TestCase):
-    def test_CL(self):
+    def test_llt_unconstrained_optimization(self):
         ### GEOMETRY DEFINITION ###
 
         """
@@ -79,9 +80,40 @@ class TestForces(TestCase):
 
         expected_result = np.array([
             20.49987286, 29.96135264, 36.43948293, 41.29808066, 45.05245163,
-            47.95906141, 50.16407573, 51.75658607, 52.79171779, 53.3017827 ,
-            53.3017827 , 52.79171779, 51.75658607, 50.16407573, 47.95906141,
+            47.95906141, 50.16407573, 51.75658607, 52.79171779, 53.3017827,
+            53.3017827, 52.79171779, 51.75658607, 50.16407573, 47.95906141,
             45.05245163, 41.29808066, 36.43948293, 29.96135264, 20.49987286
         ])
 
         assert_almost_equal(expected_result, gamma_magnitude)
+
+        V_induced = calc_induced_velocity(v_ind_coeff, gamma_magnitude)
+        V_app_fs = V_app_infs - V_induced
+        spans = np.array([panel.get_panel_span() for panel in panels1D])
+        Thrust_inviscid_per_Panel = V_app_fs[:, 1] * gamma_magnitude * spans * rho
+        Thrust_inviscid_total = sum(Thrust_inviscid_per_Panel) / 2  # half of the sail is mirrored in the water
+
+        assert_almost_equal(680.4352293328566, Thrust_inviscid_total)
+        ### Calculate chord assuming CL
+
+        # Use polar calculated for: NACA 2415, Re = 2e6 by XFoil
+        # CL/CD max for AoA = 8 --> CL(8) = 1.0913, CD(8) = 0.01327
+        # alfa_0 = -2.0 [deg]
+        a = 2 * np.pi  # the slope of the lift coefficient versus angle of attack line is 2*PI units per radian
+        CL = 1.0913
+        CD = 0.01327
+        alfa_0 = -2.0 * np.pi / 180
+
+        alfa_app_fs = np.arctan(V_app_fs[:, 1] / V_app_fs[:, 0])
+        alfa_app_infs = np.array(
+            [winds.get_app_alfa_infs_at_h(tws_at_ctr_point) for tws_at_ctr_point in tws_at_ctr_points])
+        alfa_ind = alfa_app_infs - alfa_app_fs
+        phi = alfa_app_fs - alfa_0 - CL / a  # sail_twist eq.2.22 from GG
+        phi_deg = np.rad2deg(phi)
+
+        phi_deg_expected = np.array([-4.96381481, -4.96381481, -4.96381481, -4.96381481, -4.96381481,
+                                     -4.96381481, -4.96381481, -4.96381481, -4.96381481, -4.96381481,
+                                     -4.96381481, -4.96381481, -4.96381481, -4.96381481, -4.96381481,
+                                     -4.96381481, -4.96381481, -4.96381481, -4.96381481, -4.96381481])
+
+        assert_almost_equal(phi_deg_expected, phi_deg)